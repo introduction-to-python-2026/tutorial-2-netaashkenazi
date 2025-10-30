6d9730b01117105dc86c85a8d601f994b222d1ee
netaashkenazi <neta.ashkenazi.email@gmail.com>
2025-10-30 11:26:21 +0200

    תרגיל 2 - ניסיון 2

--- math_utils.py
+++ math_utils.py
@@ -5,3 +5,9 @@ def find_max_number(num1, num2, num3):
         return num2
     elif num3 > num1 and num3 > num2:
         return num3
+
+num1 = 1
+num2 = 3
+num3 = -1
+max_num = find_max_number(num1, num2, num3)
+print("The maximum number is:", max_num)
