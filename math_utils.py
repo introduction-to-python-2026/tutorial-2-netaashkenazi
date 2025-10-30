d56b8d335e87d3aa82d3f09edcefbf7d9886bc37
netaashkenazi <neta.ashkenazi.email@gmail.com>
2025-10-30 11:37:19 +0200

    Update math_utils.py

--- math_utils.py
+++ math_utils.py
@@ -1,13 +1,8 @@
 def find_max_number(num1, num2, num3):
     if num1 > num2 and num1 > num3 :
         return num1
-    elif num2 > num1 and num2 > num3:
+    elif num2 > num3:
         return num2
-    elif num3 > num1 and num3 > num2:
+    else :
         return num3
-
-num1 = 1
-num2 = 3
-num3 = -1
-max_num = find_max_number(num1, num2, num3)
-print("The maximum number is:", max_num)
+   
